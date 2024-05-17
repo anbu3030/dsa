e49d27974eb1351d4893c1fa0bccd8af0e0e2c7e
anbu3030 <166837753+anbu3030@users.noreply.github.com>
2024-05-17 16:24:43 +0200

    Update forloop.py

--- forloop.py
+++ forloop.py
@@ -14,3 +14,15 @@ print("question number 2")
 
 for e in range(entery,0,-1):
     print(e)
+
+print("question number 3")
+#Print the even numbers from 1 till the number entered by the user.
+
+for l in range(0,entery+1,2):
+    print(l)
+print("question number 4")
+#Print a multiplication table of a number for the first 10 multiples.
+
+
+for x in range(entery,10*entery+1,entery):
+    print(x)
