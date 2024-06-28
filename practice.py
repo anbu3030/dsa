6c5c0c0c9d74ab27867619d948ee6ba398c6aaa7
anbu3030 <166837753+anbu3030@users.noreply.github.com>
2024-06-28 16:17:52 +0200

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,18 @@
+# convert fareheit to celies
+f=int(input("fareheit"))
+
+c=f-32
+
+sec=5/9
+
+cel=c*sec
+
+print(cel)
+
+
+m=int(input("minutes"))
+
+h=m//60 
+min=m%60
+
+print("hour=",h,"\n minutes=",min)
