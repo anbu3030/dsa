31cdaaba8fbe82971550b2cd84f32c6af4af7124
anbu3030 <166837753+anbu3030@users.noreply.github.com>
2024-07-05 16:14:03 +0200

    Add files via upload

--- practice.py
+++ practice.py
@@ -1,18 +1,30 @@
-# convert fareheit to celies
-f=int(input("fareheit"))
 
-c=f-32
+def fareheit():
+    # convert fareheit to celies
+    f=int(input("fareheit"))
+    c=f-32
+    sec=5/9
+    cel=c*sec
+    print(cel)
 
-sec=5/9
+def hour():
+    m=int(input("minutes"))
+    h=m//60 
+    min=m%60
+    print("hour=",h,"\nminutes=",min)
 
-cel=c*sec
+def inches():
+    i=int(input("inches"))
+    cm=i*2.54
+    print(cm)
 
-print(cel)
-
-
-m=int(input("minutes"))
-
-h=m//60 
-min=m%60
-
-print("hour=",h,"\n minutes=",min)
+while True:
+    menu=int(input("menu\n1=fareheit to celies\n2=hours to minutes\n3=inches to cm\n4=end"))
+    if menu==1:
+        fareheit()
+    elif menu==2:
+        hour()
+    elif menu==3:
+        inches()
+    else:
+        break
