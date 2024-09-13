59350c728783fcf652831eb30c9344a5261678a9
anbu3030 <166837753+anbu3030@users.noreply.github.com>
2024-09-13 16:54:56 +0200

    Update bst.py

--- bst.py
+++ bst.py
@@ -12,6 +12,22 @@ def inordertraversal(root):
     if root.right is not None:
         inordertraversal(root.right)
 
+def preordertraversal(root):
+    print(root.key)
+    if root.left is not None:
+        preordertraversal(root.left)
+
+    if root.right is not None:
+        preordertraversal(root.right)
+
+def postordertraversal(root):
+    if root.left is not None:
+        postordertraversal(root.left)
+
+    if root.right is not None:
+        postordertraversal(root.right)
+    print(root.key)
+
 def insert(root,put):
     if root is None:
         return Node(put)
@@ -21,8 +37,37 @@ def insert(root,put):
         root.right=insert(root.right,put)
     return root
 
+def search(root,sea):
+    if root.key==sea:
+        return True
+    elif root.key<sea and root.right is not None:
+        return search(root.right,sea)
+    elif root.key>sea and root.left is not None:
+        return search(root.left,sea)
+    
+    else:
+        return False
+
+        
+    
+
+
+
+def menu():
+    m=int(input("1=inordertraversal\n2=preordertraversal\n3=postordertraversal"))
+    if m==1:
+        print(inordertraversal(tree))
+    
+    if m==2:
+        print(preordertraversal(tree))
+
+    if m==3:
+        print(postordertraversal(tree))
+
+        
 tree=None
-for i in range(8):
+for i in range(12):
     ini=int(input("insert a new value"))
     tree=insert(tree,ini)
-inordertraversal(tree)
+#menu()
+print(search(tree,11))
