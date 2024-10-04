f54d4d077548735c1ec6ddb355011e6c37dd928c
anbu3030 <166837753+anbu3030@users.noreply.github.com>
2024-10-04 17:02:23 +0200

    Update graphebfs.py

--- graphebfs.py
+++ graphebfs.py
@@ -1,3 +1,5 @@
+import Queue1
+
 class Graph:
     def __init__(self,n):
         self.n=n
@@ -9,9 +11,29 @@ class Graph:
         self.adjacency[node2].append(node1)
         print(self.adjacency)
 
-Graph1=Graph(5)
+    def bfs(self,source):
+        vis=[]
+        Qu=Queue1.Queue(self.n)
+        vis.append(source)
+        Qu.enqueue(source)
+        while Qu.lenth()>0:
+            d=Qu.dequeue()
+
+            for s in self.adjacency[d]:
+                if s not in vis:
+                    vis.append(s)
+                    Qu.enqueue(s)
+        return vis
+
+Graph1=Graph(8)
 Graph1.edge(0,1)
-Graph1.edge(0,2)
 Graph1.edge(1,2)
+Graph1.edge(2,3)
 Graph1.edge(1,3)
-Graph1.edge(4,1)
+Graph1.edge(3,4)
+Graph1.edge(1,4)
+Graph1.edge(4,5)
+Graph1.edge(1,5)
+Graph1.edge(5,6)
+Graph1.edge(6,7)
+print(Graph1.bfs(0))
